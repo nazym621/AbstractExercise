90e82c88661d4df5afd94c3e884cb20de3e06ba8
sanaz <sanazmarand@gmail.com>
2020-01-31 14:10:55 -0600

    add

---
+++ AbstractExercise/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AbstractExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mercedes = new Car();
+            mercedes.Drive();
+
+            var abugati = new Motorcyle();
+            abugati.Drive();
+            
+            
+
+
+            //Console.WriteLine($"A car drives with {Wheels} wheels and has {Doors} doors");
+        }
+    }
+}
---
+++ AbstractExercise/Vehicle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractExercise
+{
+    public abstract class Vehicle
+    {
+        public int Wheels { get; set; }
+        public int Doors { get; set; }
+        public abstract void Drive();
+        
+    }
+    public class Car : Vehicle
+    {
+        public override void Drive()
+        {
+            Wheels = 4;
+            Doors = 4;
+            Console.WriteLine($"A car drives with {Wheels} wheels and has {Doors} doors");
+        }
+    }
+    public class Motorcyle: Vehicle
+    {
+        public override void Drive()
+        {
+            Wheels = 2;
+            Doors = 0;
+            Console.WriteLine($"A motorcyle drives with {Wheels} wheels and {Doors} doors");
+        }
+    }
+}
