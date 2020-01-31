9a5bef64083c8c571d8f04f324fcc6501c59cd9b
sanaz <sanazmarand@gmail.com>
2020-01-31 14:33:01 -0600

    initial commit

--- AbstractExercise/Program.cs
+++ AbstractExercise/Program.cs
@@ -12,10 +12,8 @@ namespace AbstractExercise
             var abugati = new Motorcyle();
             abugati.Drive();
             
-            
+          
 
-
-            //Console.WriteLine($"A car drives with {Wheels} wheels and has {Doors} doors");
         }
     }
 }
